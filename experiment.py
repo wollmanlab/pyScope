2b8898ea7af3aae4d7e52370881107c6cf9b542e
zehemminger <zachary.hemminger@gmail.com>
2025-12-04 12:38:43 -0800

    Fix typo in status update for crashed state in Experiment class

--- experiment.py
+++ experiment.py
@@ -102,7 +102,7 @@ class Experiment():
             if not crashed:
                 self.status = "offline"
             else:
-                self.status = f"Crashed:{self.staus.split(':')[-1]}"
+                self.status = f"Crashed:{self.status.split(':')[-1]}"
             self.log('Continuous monitoring terminated - status set to offline')
 
     def interpret_command(self, current_message):
